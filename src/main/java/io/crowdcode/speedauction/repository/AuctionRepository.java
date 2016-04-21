916936e199b3f2c6cde97d192b3ceb8d5609054c
Ingo Düppe <ingo.dueppe@crowdcode.io>
2016-04-21 15:03:14 +0200

    Add AuctionRepository Interface

---
+++ src/main/java/io/crowdcode/speedauction/repository/AuctionRepository.java
@@ -0,0 +1,19 @@
+package io.crowdcode.speedauction.repository;
+
+import io.crowdcode.speedauction.model.Auction;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author Ingo Düppe (Crowdcode)
+ */
+public interface AuctionRepository {
+
+    Optional<Auction> find(Long auctionId);
+
+    List<Auction> findAll();
+
+    void save(Auction auction);
+
+}
