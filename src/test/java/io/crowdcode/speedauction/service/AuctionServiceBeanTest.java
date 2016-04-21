7fa1fea07f84f88dd54414976f0601288b3212b6
Ingo Düppe <ingo.dueppe@crowdcode.io>
2016-04-21 14:49:03 +0200

    Enhanced Test

--- src/test/java/io/crowdcode/speedauction/service/AuctionServiceBeanTest.java
+++ src/test/java/io/crowdcode/speedauction/service/AuctionServiceBeanTest.java
@@ -1,8 +1,15 @@
 package io.crowdcode.speedauction.service;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.format.datetime.joda.LocalDateParser;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -10,11 +17,23 @@ import static org.junit.Assert.assertNotNull;
  */
 public class AuctionServiceBeanTest {
 
+    private static final Logger log = LoggerFactory.getLogger(AuctionServiceBeanTest.class);
+
     @Test
     public void testApplicationContext() throws Exception {
+        log.info("======== Starting Test at {} ", LocalDateTime.now().toLocalTime().toString());
+
         try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("uebung-1.xml")) {
             AuctionService bean = context.getBean("auctionService", AuctionService.class);
+
+            assertTrue(context.isSingleton("auctionService"));
+
+            System.out.println(Arrays.toString(context.getBeanDefinitionNames()));
+
             assertNotNull(bean);
         }
+
+
+
     }
 }
