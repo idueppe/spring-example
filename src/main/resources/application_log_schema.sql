76fe32f2339ea17dff812a4a2a0df91d823485f9
Ingo Düppe <ingo.dueppe@crowdcode.io>
2016-04-23 23:39:11 +0200

    Fixed Persistence

--- src/main/resources/application_log_schema.sql
+++ src/main/resources/application_log_schema.sql
@@ -1,7 +1,9 @@
-DROP TABLE ApplicationLog;
+DROP TABLE Application_Log;
 DROP SEQUENCE LogSequence;
-CREATE TABLE ApplicationLog (
-  id      BIGINT PRIMARY KEY NOT NULL,
-  message CHARACTER VARYING(255)
+CREATE TABLE Application_Log (
+  id        BIGINT PRIMARY KEY NOT NULL,
+  message   CHARACTER VARYING(2048),
+  createdBy CHARACTER VARYING(100),
+  createdAt TIMESTAMP
 );
 CREATE SEQUENCE LogSequence;
